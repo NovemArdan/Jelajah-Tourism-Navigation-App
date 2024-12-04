1ab981cdc0afd015b8fa8dad23e18f98482b25cb
NovemArdan <novemrohmadin@gmail.com>
2024-12-04 12:41:40 +0700

    add nfc read

--- app/src/main/java/com/example/jelajah3/MainActivity.kt
+++ app/src/main/java/com/example/jelajah3/MainActivity.kt
@@ -1,24 +1,149 @@
 package com.example.jelajah3
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.IntentFilter
+import android.nfc.NdefMessage
+import android.nfc.NfcAdapter
+import android.nfc.tech.NfcA
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.ui.setupWithNavController
+import com.example.jelajah3.ui.nfctag.NFCTagFragment
+import android.nfc.Tag
+
 
 class MainActivity : AppCompatActivity() {
+
+    var nfcAdapter: NfcAdapter? = null
+    private val TAG = "NFCTagFragment"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate: Started.")
 
         val navController = findNavController(R.id.nav_host_fragment)
         findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(navController)
 
-        // Mengirim username ke HomeFragment
         val username = intent.getStringExtra("username") ?: ""
         val bundle = Bundle().apply {
             putString("username", username)
         }
         navController.setGraph(R.navigation.nav_graph, bundle)
+
+        setupNFC()
+    }
+
+    private fun setupNFC() {
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        if (nfcAdapter == null) {
+            Log.e(TAG, "setupNFC: NFC is not supported on this device.")
+            return
+        }
+
+        if (!nfcAdapter!!.isEnabled) {
+            Log.e(TAG, "setupNFC: NFC is disabled.")
+        } else {
+            Log.d(TAG, "setupNFC: NFC is enabled.")
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: Activity resumed.")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: Activity paused.")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: Activity is being destroyed.")
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        //Log.d(TAG, "Intent: ${intent}")
+        Log.d(TAG, "New intent received with action: ${intent.action}")
+
+        Log.d(TAG, "New NfcAdapter.ACTION_NDEF_DISCOVERED received: ${NfcAdapter.ACTION_NDEF_DISCOVERED}")
+        Log.d(TAG, "New NfcAdapter.ACTION_TECH_DISCOVERED received: ${NfcAdapter.ACTION_TECH_DISCOVERED}")
+        Log.d(TAG, "New NfcAdapter.ACTION_TAG_DISCOVERED received: ${NfcAdapter.ACTION_TAG_DISCOVERED}")
+
+        //if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent.action || NfcAdapter.ACTION_TECH_DISCOVERED == intent.action) {
+        if (NfcAdapter.ACTION_TAG_DISCOVERED == intent.action) {
+            Log.d(TAG, "sebelum masuk handle intent")
+            handleNfcIntent(intent)
+        } else {
+            Log.e(TAG, "Unhandled NFC Intent Action: ${intent.action}")
+        }
+    }
+
+    private fun handleNfcIntent(intent: Intent) {
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+        val currentFragment = navHostFragment?.childFragmentManager?.fragments?.first()
+
+        val tag: Tag? = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
+        if (tag != null) {
+            try {
+                // Mencoba membaca sebagai NDEF terlebih dahulu
+                val nfcMessages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+                if (nfcMessages != null) {
+                    Log.d(TAG, "NFC Messages received: ${nfcMessages}")
+                    val ndefMessages = nfcMessages.map { it as NdefMessage }
+                    for (message in ndefMessages) {
+                        for (record in message.records) {
+                            val payload = String(record.payload, Charsets.UTF_8) // Decoding payload
+                            Log.d(TAG, "Read payload: $payload")
+                        }
+                    }
+                } else {
+                    Log.e(TAG, "No NDEF messages found.")
+                }
+
+                // Membaca informasi dasar dari tag seperti ID
+                val id = tag.id.joinToString(separator = ":") { String.format("%02X", it) }
+                Log.d(TAG, "Tag ID: $id")
+
+                // Contoh untuk membaca menggunakan NfcA
+                val nfcA = NfcA.get(tag)
+                nfcA.connect()
+                val atqa = nfcA.atqa.joinToString(":") { String.format("%02X", it) }
+                val sak = String.format("%02X", nfcA.sak)
+                Log.d(TAG, "ATQA: $atqa, SAK: $sak")
+                nfcA.close()
+
+            } catch (e: Exception) {
+                Log.e(TAG, "Error processing NFC tag", e)
+            }
+        } else {
+            Log.e(TAG, "NFC Tag is null")
+        }
+
+        if (currentFragment is NFCTagFragment) {
+            Log.d("NFCTagFragment", "Forwarding NFC intent to NFCTagFragment")
+            currentFragment.processNfcIntent(intent)
+        } else {
+            Log.e("NFCTagFragment", "Current fragment is not NFCTagFragment")
+        }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+        val currentFragment = navHostFragment?.childFragmentManager?.fragments?.first()
+
+        if (currentFragment is NFCTagFragment) {
+            Log.d("NFCTagFragment", "Forwarding NFC intent to NFCTagFragment")
+            currentFragment.processNfcIntent(intent)
+        } else {
+            Log.e("NFCTagFragment", "Current fragment is not NFCTagFragment")
+        }
     }
 }
--- app/src/main/java/com/example/jelajah3/ui/theme/nfctag/NFCTagFragment.kt
+++ app/src/main/java/com/example/jelajah3/ui/theme/nfctag/NFCTagFragment.kt
@@ -1,19 +1,32 @@
 package com.example.jelajah3.ui.nfctag
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.IntentFilter
+import android.app.PendingIntent
+import android.nfc.NdefMessage
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import com.example.jelajah3.databinding.FragmentNfctagBinding
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import android.nfc.NfcAdapter
+import android.util.Log
+import com.example.jelajah3.MainActivity
+import android.nfc.Tag
+import android.nfc.tech.NfcA
+import android.os.Build
+import java.io.IOException
 
 class NFCTagFragment : Fragment() {
     private var _binding: FragmentNfctagBinding? = null
     private val binding get() = _binding!!
-    private val viewModel: NFCTagViewModel by viewModels()
+
+    private val nfcAdapter: NfcAdapter?
+        get() = (activity as MainActivity).nfcAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,27 +37,103 @@ class NFCTagFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("SetTextI18n")
     private fun setupInteraction() {
         binding.imageViewScan.setOnClickListener {
-            // Check if NFC is available or enabled, you might need to add this logic in ViewModel
-            if (viewModel.isNFCEnabled()) {
-                showScanningDialog()
+            Log.d("NFCTagFragment", "ImageView clicked")
+            binding.textViewPrompt.text = "Proses scan lokasi sedang berjalan\ndekatkan gawai anda dengan nfc tag"
+            Log.d("NFCTagFragment", "Text set on TextView")
+
+            try {
+                enableNfcForegroundDispatch()
+                Log.d("NFCTagFragment", "NFC Foreground Dispatch enabled")
+            } catch (e: Exception) {
+                Log.e("NFCTagFragment", "Failed to enable NFC Foreground Dispatch", e)
+            }
+
+//            Handler(Looper.getMainLooper()).postDelayed({
+//                try {
+//                    disableNfcForegroundDispatch()
+//                    Log.d("NFCTagFragment", "NFC Foreground Dispatch disabled")
+//                } catch (e: Exception) {
+//                    Log.e("NFCTagFragment", "Failed to disable NFC Foreground Dispatch", e)
+//                }
+//            }, 30000)
+        }
+    }
+
+    private fun enableNfcForegroundDispatch() {
+        nfcAdapter?.let { adapter ->
+            if (adapter.isEnabled) {
+                Log.d("NFCTagFragment", "masuk enableNfcForegroundDispatch")
+                val nfcIntentFilter = arrayOf(
+                    IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED),
+                    IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED),
+                    IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED)
+                )
+
+                val intent = Intent(activity, activity?.javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                val pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    PendingIntent.getActivity(
+                        activity,
+                        0,
+                        intent,
+                        //PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                        PendingIntent.FLAG_MUTABLE
+                    )
+                } else {
+                    PendingIntent.getActivity(
+                        activity,
+                        0,
+                        intent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                    )
+                }
+
+                adapter.enableForegroundDispatch(
+                    activity, pendingIntent, nfcIntentFilter, null
+                )
+                Log.d("NFCTagFragment", "NFC Foreground Dispatch enabled")
             } else {
-                Toast.makeText(context, "NFC is not available or not enabled.", Toast.LENGTH_SHORT).show()
+                Log.e("NFCTagFragment", "NFC is disabled.")
             }
         }
     }
 
-    private fun showScanningDialog() {
-        MaterialAlertDialogBuilder(requireContext())
-            .setTitle("Proses Scanning...")
-            .setMessage("Dekatkan smartphone anda ke alat tagging")
-            .setNegativeButton("Keluar") { dialog, _ ->
-                dialog.dismiss()
-            }
-            .setCancelable(false)
-            .show()
+    private fun disableNfcForegroundDispatch() {
+        nfcAdapter?.disableForegroundDispatch(activity)
     }
+
+    @SuppressLint("SetTextI18n")
+    fun processNfcIntent(intent: Intent) {
+        Log.d("NFCTagFragment", "Intent received with action: ${intent.action}")
+        val tag: Tag? = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
+
+        if (tag != null) {
+            // Convert tag ID to a hex string
+            val tagId = tag.id.joinToString(":") { byte -> "%02X".format(byte) }
+            var locationMessage = "Anda sekarang berada di\n$tagId" // Default message
+
+            // Check specific tag IDs and update location message accordingly
+            when (tagId) {
+                "05:89:91:56:BD:82:00" -> locationMessage = "Anda sedang berada di Fakultas Teknik UGM"
+                "63:E4:A6:ED" -> locationMessage = "Anda sedang berada di Perpustakaan FT UGM"
+            }
+
+            // Update the UI on the main thread
+            activity?.runOnUiThread {
+                binding.textViewPrompt.text = "Proses scan lokasi sedang berjalan\ndekatkan gawai anda dengan nfc tag\n$locationMessage"
+            }
+            Log.d("NFCTagFragment", "Tag ID: $tagId") // Log the tag ID
+        } else {
+            // Update the UI and log if no tag is detected
+            activity?.runOnUiThread {
+                binding.textViewPrompt.text = "NFC Tag is null"
+            }
+            Log.e("NFCTagFragment", "NFC Tag is null")
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
